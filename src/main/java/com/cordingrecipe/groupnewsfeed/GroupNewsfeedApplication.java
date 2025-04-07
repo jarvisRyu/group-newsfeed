a9c0e7373adaa8c6faaafd3ffa71ed42740091c9
류형철 <fbgudcjf2@gmail.com>
2025-04-07 12:38:41 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/cordingrecipe/groupnewsfeed/GroupNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.cordingrecipe.groupnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupNewsfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupNewsfeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cordingrecipe/groupnewsfeed/GroupNewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordingrecipe.groupnewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupNewsfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
