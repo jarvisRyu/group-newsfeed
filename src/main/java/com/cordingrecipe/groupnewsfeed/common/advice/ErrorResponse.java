486643db9829846489d37e79ff28c6709c0dc8dc
류형철 <fbgudcjf2@gmail.com>
2025-04-08 17:39:41 +0900

    feat(exception): ErrorResponse 추가

---
+++ src/main/java/com/cordingrecipe/groupnewsfeed/common/advice/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.cordingrecipe.groupnewsfeed.common.advice;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)//null 값은 Json에 포함시키지 않음.
+public class ErrorResponse {
+
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int status; //상태코드
+    private final String error; //상태코드 메세지
+    private final String message;
+
+    public ErrorResponse(int status, String error,String message) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
+
+}
+
