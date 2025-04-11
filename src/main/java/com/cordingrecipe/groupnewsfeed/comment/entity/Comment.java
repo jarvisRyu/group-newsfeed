92d6a437c7557fe63599563ca828dbe3a5b78d08
hosung77 <ho4728@gmail.com>
2025-04-12 03:11:27 +0900

    refactor:피드백 받은 내용 수정

--- src/main/java/com/cordingrecipe/groupnewsfeed/comment/entity/Comment.java
+++ src/main/java/com/cordingrecipe/groupnewsfeed/comment/entity/Comment.java
@@ -17,7 +17,7 @@ public class Comment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long commentId;
+    private Long id;
 
     @Column(nullable = false)
     private String commentContent;
